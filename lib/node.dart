412e639a07a5c1700f70b4f0767f98d1b74ddc3e
Hamed Rezaee <57184669+hamed-deriv@users.noreply.github.com>
2023-09-13 19:47:04 +0800

    add path finder class

--- lib/node.dart
+++ lib/node.dart
@@ -13,4 +13,28 @@ class Node {
   double f = 0;
   double g = 0;
   double h = 0;
+
+  Node clone() => Node(position)
+    ..visited = visited
+    ..isWall = isWall
+    ..previous = previous
+    ..f = f
+    ..g = g
+    ..h = h;
+
+  static List<List<Node>> cloneList(List<List<Node>> list) {
+    final List<List<Node>> newList = <List<Node>>[];
+
+    for (final List<Node> nodes in list) {
+      final List<Node> row = <Node>[];
+
+      for (final Node node in nodes) {
+        row.add(node.clone());
+      }
+
+      newList.add(row);
+    }
+
+    return newList;
+  }
 }
